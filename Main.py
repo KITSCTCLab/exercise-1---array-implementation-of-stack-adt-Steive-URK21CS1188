0845c1af0ff6ab585959c44c6e84aa144ab9fb3f
Steive-URK21CS1188 <110010600+Steive-URK21CS1188@users.noreply.github.com>
2022-07-26 10:59:01 +0530

    Initial commit

--- Main.py
+++ Main.py
@@ -6,20 +6,37 @@ class Stack:
 
     def is_empty(self):
         # Write code here
+        if self.top == -1:
+            return 1
+        else :
+            return 0
 
     def is_full(self):
         # Write code here
+        if self.top == (self.size - 1):
+            return 1
+        else :
+            return 0
 
     def push(self, data):
         if not self.is_full():
             # Write code here
+            self.top+=1
+            self.lst[self.top]=data
 
     def pop(self):
         if not self.is_empty():
             # Write code here
+            t=self.lst[self.top]
+            del self.lst[self.top]
+            self.top-=1
+            return t
 
     def status(self):
         # Write code here
+         if not self.isEmpty():
+            for i in range(0,self.top+1):
+                print(self.lst[i])
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
